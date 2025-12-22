5cba9ee3a6347fc22fdf3c70a7c3a220c0a55d87
adm <kaio.candf@gmail.com>
2025-12-22 15:44:39 -0300

    configurado o arquivode configuração de banco de dados

---
+++ src/main/java/oi/githubkaiocandido/libraryapi/config/DataBaseConfiguration.java
@@ -0,0 +1,54 @@
+package oi.githubkaiocandido.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfiguration {
+
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    /*@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }*/
+
+    @Bean
+    public DataSource hikariDataSource(){
+        HikariConfig hk = new HikariConfig();
+        hk.setJdbcUrl(url);
+        hk.setUsername(username);
+        hk.setPassword(password);
+        hk.setDriverClassName(driver);
+
+        hk.setMaximumPoolSize(10); // maximo de conexões liberadas
+        hk.setMinimumIdle(1); // minimo de conexões liberadas
+        hk.setPoolName("Library"); // nome
+        hk.setMaxLifetime(600000); // 600 mil ms (tempo de duração de conexão)
+        hk.setConnectionTimeout(100000); // tempo para obter conexão
+        hk.setConnectionTestQuery("select 1"); // teste de conexão no banco
+
+        return new HikariDataSource(hk);
+
+    }
+
+}
