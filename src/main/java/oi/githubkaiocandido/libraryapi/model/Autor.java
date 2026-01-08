9f72563ccd41b79781cfd827e3e17526f48dddc0
adm <kaio.candf@gmail.com>
2026-01-08 15:15:41 -0300

    ajustado no model Autor, o mapeamento nacionalidade

--- src/main/java/oi/githubkaiocandido/libraryapi/model/Autor.java
+++ src/main/java/oi/githubkaiocandido/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 @Entity
@@ -13,6 +17,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "livros")
+@EntityListeners(AuditingEntityListener.class)
 public class Autor {
 
     @Id
@@ -25,9 +30,20 @@ public class Autor {
     @Column(name = "data_nascimento", nullable = false)
     private LocalDate dataNascimento;
 
-    @Column(name = "nascionalidade", length = 50, nullable = false)
+    @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nascionalidade;
 
     @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
     private List<Livro> livros;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 }
