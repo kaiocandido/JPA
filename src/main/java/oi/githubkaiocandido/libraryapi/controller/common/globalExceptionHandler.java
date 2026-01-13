4d1ca979433d2d89a29921248eedf9cc04793bdf
adm <kaio.candf@gmail.com>
2026-01-13 10:29:41 -0300

    ajuste de imports

--- src/main/java/oi/githubkaiocandido/libraryapi/controller/common/globalExceptionHandler.java
+++ src/main/java/oi/githubkaiocandido/libraryapi/controller/common/globalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
