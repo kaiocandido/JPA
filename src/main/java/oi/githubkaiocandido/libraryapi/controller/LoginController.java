ccee5def1b4d0df3494d448780937030ae33ecf3
adm <kaio.candf@gmail.com>
2026-01-20 16:49:41 -0300

    aprendendo spring security autenticação

---
+++ src/main/java/oi/githubkaiocandido/libraryapi/controller/LoginController.java
@@ -0,0 +1,13 @@
+package oi.githubkaiocandido.libraryapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
+}
