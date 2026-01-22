e5db417d44f91dd02581b0d3b543b72af17b2842
adm <kaio.candf@gmail.com>
2026-01-22 16:42:29 -0300

    criando model de usuarios e ajustando roles com biblioteca hypersistence

---
+++ src/main/java/oi/githubkaiocandido/libraryapi/model/Usuario.java
@@ -0,0 +1,32 @@
+package oi.githubkaiocandido.libraryapi.model;
+
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column
+    private String login;
+    @Column
+    private String password;
+    @Type(ListArrayType.class)
+    @Column(name = "roles", columnDefinition = "varchar[]")
+    private List<String> roles;
+}
